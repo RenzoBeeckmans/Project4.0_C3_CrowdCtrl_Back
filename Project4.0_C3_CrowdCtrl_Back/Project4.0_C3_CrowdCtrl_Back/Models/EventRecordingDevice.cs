534cdce7074066cca8ec73512b005578c991dfcf
JxVdV <vandeveljari@outlook.com>
2023-01-19 13:01:58 +0100

    added new entities

--- Project4.0_C3_CrowdCtrl_Back/Project4.0_C3_CrowdCtrl_Back/Models/EventRecordingDevice.cs
+++ Project4.0_C3_CrowdCtrl_Back/Project4.0_C3_CrowdCtrl_Back/Models/EventRecordingDevice.cs
@@ -1,6 +1,20 @@
-﻿namespace Project4._0_C3_CrowdCtrl_Back.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Project4._0_C3_CrowdCtrl_Back.Models
 {
     public class EventRecordingDevice
     {
+        public int EventRecordingDeviceId { get; set; }
+        public string PlacementName { get; set;}
+        public int ZoneId { get; set;}
+        public int RecordingDeviceId { get; set; }
+        public int EventId { get; set; }
+
+        [JsonIgnore]
+        public Zone Zone { get; set; }
+        [JsonIgnore]
+        public RecordingDevice RecordingDevice { get; set; }
+        [JsonIgnore]
+        public Event Event { get; set; }
     }
 }
--- Project4.0_C3_CrowdCtrl_Back/Project4.0_C3_CrowdCtrl_Back/Models/MoodType.cs
+++ Project4.0_C3_CrowdCtrl_Back/Project4.0_C3_CrowdCtrl_Back/Models/MoodType.cs
@@ -1,6 +1,14 @@
-﻿namespace Project4._0_C3_CrowdCtrl_Back.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Project4._0_C3_CrowdCtrl_Back.Models
 {
     public class MoodType
     {
+        public int MoodTypeId { get; set; }
+        public string Name { get; set; }
+        [JsonIgnore]
+        public List<Mood>? Moods { get; set; } = new List<Mood>();
+        [JsonIgnore]
+        public List<Incident>? Incidents { get; set; } = new List<Incident>();
     }
 }
